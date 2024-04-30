ae058643e53bb6321bc08a29a009ce83606745c9
efrinn <164054699+efrinn@users.noreply.github.com>
2024-04-30 11:27:20 -0600

    Add files via upload

---
+++ ejemploFor3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 100
+
+int vector[MAX];
+int pos=0;
+
+void agregar(int num);
+void mostrar ();
+
+int main(int argc, char const *argv[])
+{
+    int cant ;
+    int num ;
+    cout << " dime la cantidad que deseas almacenar";
+    cin >> cant ;
+    for (int i=0; i<cant; i++){
+        cout << "agregar numero que deseas almaenar";
+        cin >> num;
+        agregar (num);
+    }
+    cout << "mostrar numero/n";
+    mostrar();
+    return 0;
+}
+
+void agregar(int num){
+    vector[pos]= num;
+    pos++;
+}
+
+void mostrar(){
+    for(int i =0; i<pos; i++){
+        cout << vector[i] << endl;
+    }
+}
